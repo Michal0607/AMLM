a4751a123a4844198f2172e811c752b232f74d6a
Mich <michal.szulierz.kontakt@gmail.com>
2024-04-27 21:51:24 +0200

    Update results

---
+++ evaluate.py
@@ -0,0 +1,93 @@
+import numpy as np
+import pandas as pd 
+import keras
+from sklearn.metrics.pairwise import cosine_similarity
+from keras.models import load_model
+import os
+
+def test_data_generator(path,limit):
+    list_temp = []
+    current_speaker, speakers_count, check = 0, 0, 0
+
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(".npy"):
+                file_path = os.path.join(root, file)
+                filename = os.path.splitext(file)[0]
+                speaker_id = int(filename.split('_')[0])
+
+                if speaker_id != current_speaker:
+                    current_speaker = speaker_id
+                    speakers_count += 1
+                    check = 0
+
+                mfcc_matrix = np.load(file_path)
+                if mfcc_matrix.shape[0] == 157:
+                    if speakers_count > limit:
+                        list_temp.append(mfcc_matrix) # 8
+                        if len(list_temp) == 10:
+                            X_enrollment = np.concatenate(list_temp, axis=0)
+                            y_enrollment = speaker_id
+                            list_temp = []
+                            check = 1
+                            yield X_enrollment, y_enrollment, 'enrollment'
+                        if len(list_temp) == 6 and check == 1: # 2
+                            X_test = np.concatenate(list_temp, axis=0)
+                            y_test = speaker_id
+                            list_temp = []
+                            yield X_test, y_test, 'test'
+
+def genuine_impost_test(test_sample,embedding_layer,mean_enrollment_embedding,j,i,test,scores):
+        test_embeddings=[]
+        test_samples_157=[test_sample[k:k+157] for k in range(0,len(test_sample),157)]
+        for test_sample_157 in test_samples_157:
+            test_embedding=embedding_layer.predict(np.expand_dims(test_sample_157,axis=0))[0,:]
+            test_embeddings.append(test_embedding)
+
+        mean_test_embedding=np.expand_dims(np.mean(test_embeddings,axis=0),axis=0)
+        similarity_score=cosine_similarity(mean_enrollment_embedding,mean_test_embedding)
+        is_genuine=test
+        scores.append([y_enrollment[j],y_test[i],similarity_score[0][0],is_genuine])
+
+def score(embedding_layer,X_enrollment,y_enrollment,X_test,y_test,output_csv):
+    scores=[]
+    print(type(scores))
+    for j in range(len(X_enrollment)):
+        enrollment_embeddings =[]
+        enrollment_sample = X_enrollment[j]
+        enrollment_samples_157 = [enrollment_sample[k:k+157] for k in range(0, len(enrollment_sample), 157)]
+        for enrollment_sample_157 in enrollment_samples_157:
+            enrollment_embedding = embedding_layer.predict(np.expand_dims(enrollment_sample_157, axis=0))[0,:]
+            enrollment_embeddings.append(enrollment_embedding)
+        mean_enrollment_embedding = np.expand_dims(np.mean(enrollment_embeddings, axis=0),axis=0)
+
+        for i in range(len(X_test)):
+            test_sample = X_test[i]
+            random=round(np.random.uniform(0,1),2)
+            
+            if y_enrollment[j]==y_test[i] and random<=0.7:
+                genuine_impost_test(test_sample,embedding_layer,mean_enrollment_embedding,j,i,1,scores)
+            elif y_enrollment[j]!=y_test[i] and random<=0.12:
+                genuine_impost_test(test_sample,embedding_layer,mean_enrollment_embedding,j,i,0,scores)
+        print(f"Próbka numer: {j}")
+    df = pd.DataFrame(scores,columns=['Speaker A','Speaker B','Score','Test'])
+    df.to_csv(output_csv,index=False)
+
+if __name__ == '__main__':
+    path = "C:/Users/48502/Desktop/Baza/IAD2SEM1/dataset5s_mfcc"
+    limit = 200
+    X_enrollment,X_test,y_enrollment,y_test = [],[],[],[]
+
+    data_generator = test_data_generator(path, limit)
+
+    for X, y, dataset_type in data_generator:
+        if dataset_type == 'enrollment':
+            X_enrollment.append(X)
+            y_enrollment.append(y)
+        elif dataset_type == 'test':
+            X_test.append(X)
+            y_test.append(y)
+    model=load_model('AMLM/model.keras')
+    model.summary()
+    embedding_layer=keras.Model(inputs=model.layers[0].input,outputs=model.layers[3].output)
+    score(embedding_layer,X_enrollment,y_enrollment,X_test,y_test,'AMLM/results_3.csv')
--- model.py
+++ model.py
@@ -1,41 +1,90 @@
 import numpy as np
-from keras.layers import Dense,Dropout,LSTM,Sequential,BatchNormalization
+from keras.layers import Dense,Dropout,LSTM,BatchNormalization
+from keras.models import Sequential
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-import librosa
+import matplotlib.pyplot as plt
+import seaborn as sns
+import keras
+from keras_tuner import RandomSearch, HyperParameters
 import os
 
-def train_data(path,limit_speaker,limit_files=1000):
-    X_train,y_train=[],[]
+def train_data_generator(path, limit_speaker):
     files=os.listdir(path)
-    i,j=0,0
-    while i<limit_speaker:
-        id=files[0].split("_")[0]
-        for file in files:
-            if id!=file.split("_")[0] or j>=limit_files:
-                if id==file.split("_")[0]:
-                    continue
-                i+=1
-                j=0
-            audio=np.load(os.path.join(path,file))
-            id=file.split("_")[0]
-            X_train.append(audio)
-            y_train.append(id)
-            j+=1
+    i=0
+    current_id=files[0].split("_")[0]
 
-    return np.array(X_train),np.array(y_train)
+    for file in files:
+        if current_id!=file.split("_")[0]:
+            i+=1
+            if i>=limit_speaker:
+                break
 
-def model():
-    model=Sequential(
-        LSTM(160,input_shape=(157,13),return_sequences=True),
-        LSTM(128),
-        Dense(64),
-        Dense(50)
-    )
-    model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+        audio=np.load(os.path.join(path,file))
+        current_id=file.split("_")[0]
+        yield audio,current_id
+
+def get_model():
+    model=Sequential([
+        LSTM(384,input_shape=(157,13)),
+        Dense(256,activation='relu'),
+        Dropout(0.25),
+        Dense(96,activation='relu'),
+        Dense(200,activation='softmax')
+    ])
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0003),loss='categorical_crossentropy',metrics=['accuracy'])
 
     return model
 
-if __name__=="__main__":
-    path="C:/Users/48502/Desktop/Baza/IAD2SEM1/dataset5s_mfcc"
-    X_train,y_train=train_data(path,200,150)
+def history_plot(history):
+    loss=history.history['loss']
+    val_loss=history.history['val_loss']
+    accuracy=history.history['accuracy']
+    val_accuracy=history.history['val_accuracy']
+    
+    sns.set(style="whitegrid")  
+    
+    plt.figure(figsize=(16,6))
+
+    plt.subplot(1,2,1)
+    sns.lineplot(x=range(1,len(loss)+1),y=loss,label='Train Loss',marker='o')  
+    sns.lineplot(x=range(1,len(val_loss)+1),y=val_loss,label='Validation Loss',marker='o')
+    plt.legend()
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Loss Over Epochs')
+
+    plt.subplot(1,2,2)
+    sns.lineplot(x=range(1,len(accuracy)+1),y=accuracy,label='Train Accuracy',marker='o')
+    sns.lineplot(x=range(1,len(val_accuracy)+1),y=val_accuracy,label='Validation Accuracy',marker='o')
+    plt.legend()
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.title('Accuracy Over Epochs')
+    
+    plt.tight_layout()  
+    plt.show()
+
+if __name__ == "__main__":
+    path = "C:/Users/48502/Desktop/Baza/IAD2SEM1/dataset5s_mfcc"
+    generator = train_data_generator(path,200)
+    X_train,y_train=[],[]
+    for audio, speaker_id in generator:
+        X_train.append(audio)
+        y_train.append(speaker_id)
+
+    X_train=np.array(X_train)
+    y_train=np.array(y_train)
+    encoder=LabelEncoder()
+    y_train=encoder.fit_transform(y_train)
+    num_speakers=len(encoder.classes_)
+    y_train=keras.utils.to_categorical(y_train,num_classes=num_speakers)
+    print(X_train.shape,y_train.shape)
+
+    X_train,X_val,y_train,y_val=train_test_split(X_train,y_train,test_size=0.2,random_state=42)
+
+    model=get_model()
+    model.summary()
+    history=model.fit(X_train,y_train,epochs=25,batch_size=32,validation_data=(X_val,y_val))
+    history_plot(history)
+    model.save('AMLM/model.keras')
